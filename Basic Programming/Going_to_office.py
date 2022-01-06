6db9e99e93cdef2e9d6400b26b259483fa8bd8b7
sahilsimform <sahilsinghsimform@gmail.com>
2022-01-06 13:41:02 +0530

    adding km = int(input()) file

---
+++ Basic Programming/Going_to_office.py
@@ -0,0 +1,12 @@
+
+km = int(input())
+c, f, d = map(int, input().split())
+online_cost = c + ((km-f)*d)
+
+s, b, m, d = map(int, input().split())
+classic_cost = b + ((km//s)*m) + (km*d)
+
+if online_cost<=classic_cost:
+    print('Online Taxi')
+else:
+    print('Classic Taxi')
