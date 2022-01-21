7469bcbf4c72f5e6d0aa9a7e103b8e1c564d6c89
sahilsimform <sahilsinghsimform@gmail.com>
2022-01-21 16:12:55 +0530

    Adding the Coding Files

---
+++ Basic Programming/Week2/Disk_Tower.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int c=n;
+    int a[n+1]={0};
+    for(int i=0;i<n;i++)
+    {
+        int x;
+        cin>>x;
+        a[x]=1;
+        if(x==c)
+        while(a[c]==1)
+        {
+            cout<<c<<" ";
+            c--;
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
+
---
+++ Basic Programming/Week2/Vowel.py
@@ -0,0 +1,14 @@
+N=int(input())
+s=input()
+dict={"a":0,"e":0,"i":0,"o":0,"u":0}
+for i in s:
+    if i in dict:
+        dict[i]=1
+count=0
+for i in dict:
+     if dict[i]==1:
+          count+=1
+if count == 5:
+     print("YES")
+else:
+     print("NO")
